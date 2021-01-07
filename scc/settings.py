42524a9a1fcc889d7272e7b3264bffd55b08a73b
Vishwajeet Sharma <vishwajeet21sharma@gmail.com>
2021-01-07 21:58:51 +0530

    Final touch on version 1.0 version completed

--- scc/settings.py
+++ scc/settings.py
@@ -131,4 +131,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'messagefromscc@gmail.com'
-EMAIL_HOST_PASSWORD = 'Pooja@124'
+EMAIL_HOST_PASSWORD = 'ShuklaCommerceClasses@124'
