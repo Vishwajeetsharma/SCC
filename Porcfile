db041b2dedb3b8ce92db2ac6928ce3fb2f348563
Vishwajeet Sharma <vishwajeet21sharma@gmail.com>
2021-01-14 21:37:14 +0530

    Commit to upload

---
+++ Porcfile
@@ -0,0 +1,1 @@
+web: gunicorn scc.wsgi --log-file -
--- scc/settings.py
+++ scc/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'g3@+#nwg2t6d*4a57!z-jo)@%80icu5xs6&enxs*k(2s&d2qxn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['enigmatic-ravine-71042.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/static/',
