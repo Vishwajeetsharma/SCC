f284ef7095b573d34827363a118c5799bb6311a1
Vishwajeet Sharma <vishwajeetsharma9821593401@gamil.com>
2021-03-03 21:32:06 +0530

    final maybe

--- scc/settings.py
+++ scc/settings.py
@@ -118,10 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/static/',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/static/',
+# ]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # SMTP configration
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
